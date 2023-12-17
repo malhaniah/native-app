21cf0c083493cd32cfd524f8c248d9d606f7f425
= <=>
2023-12-17 12:27:18 +0300

    added model component

--- App.tsx
+++ App.tsx
@@ -1,44 +1,56 @@
 import { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  TextInput,
-  ScrollView,
-} from "react-native";
+import { Button, StyleSheet, View } from "react-native";
+
+import Input from "./components/Input";
+import DisplayList from "./components/DisplayList";
 
 export default function App() {
+  interface IGoal {
+    text: string;
+    key: string;
+  }
+
   const [goal, setGoal] = useState<string>("");
-  const [courseGoal, setCourseGoal] = useState<Array<string>>([]);
+  const [courseGoal, setCourseGoal] = useState<Array<IGoal>>([]);
+  const [modelVisible, setModelVisible] = useState<boolean>(false);
 
   const handleInput = (target: string) => {
     setGoal(target);
   };
 
   const handleAddGoal = () => {
-    setCourseGoal((prevCourseGoal) => [...prevCourseGoal, goal]);
+    setCourseGoal((prevCourseGoal) => [
+      ...prevCourseGoal,
+      { text: goal, key: Math.random().toString() },
+    ]);
+    setGoal("");
+    handleModelVisible();
+  };
+
+  const handleDeleteGoal = (id: string) => {
+    setCourseGoal((currentCourseGoal) => {
+      return currentCourseGoal.filter((goal) => goal.key !== id.toString());
+    });
+  };
+
+  const handleModelVisible = () => {
+    setModelVisible((prevModelVisible) => !prevModelVisible);
   };
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.inputs}
-          placeholder="Enter your goal"
-          onChangeText={handleInput}
-        />
-        <Button title="Add Goal" onPress={handleAddGoal} />
-      </View>
-      <View shouldRasterizeIOS style={styles.goalsContainer}>
-        <ScrollView>
-          {courseGoal.map((goal, index) => (
-            <View key={index} style={styles.goalWrapper}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
-      </View>
+      <Button title="Add New Goal" onPress={handleModelVisible} />
+      <Input
+        onAddItem={handleAddGoal}
+        onEnterInput={handleInput}
+        onCancel={handleModelVisible}
+        text={goal}
+        modelVisible={modelVisible}
+      />
+      <DisplayList
+        deleteFunctionHandler={handleDeleteGoal}
+        ListItem={courseGoal}
+      />
     </View>
   );
 }
@@ -48,38 +60,5 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 20,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    // backgroundColor: "red",
-    justifyContent: "space-between",
-    alignItems: "center",
-    borderBottomWidth: 1,
-    flex: 1,
-    marginBottom: 20,
-    borderBottomColor: "#ccc",
-  },
-  inputs: {
-    width: "70%",
-    height: 50,
-    fontSize: 16,
-    marginRight: 10,
-    paddingLeft: 10,
-    paddingRight: 10,
-    borderWidth: 1,
-    borderColor: "#ccc",
-  },
-  goalsContainer: {
-    flex: 4,
-  },
-  goalWrapper: {
-    backgroundColor: "#ccc",
-    padding: 10,
-    marginBottom: 10,
-    borderRadius: 5,
-  },
-  goalText: {
-    fontSize: 16,
-    color: "#111",
   },
 });
